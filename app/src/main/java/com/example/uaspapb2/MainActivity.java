e04495cbbae13a80b79607e306165836a36f4227
asus <test@gmail.com>
2023-06-21 18:32:10 +0700

    second commit

--- app/src/main/java/com/example/uaspapb2/MainActivity.java
+++ app/src/main/java/com/example/uaspapb2/MainActivity.java
@@ -10,16 +10,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Canvas mCanvas;
-    private Paint mPaint = new Paint();
-    private Bitmap mBitmap;
-    private Rect mRect = new Rect();
-    private Rect Mbounds = new Rect();
-
-    private static final int OFFSET = 50;
-    private int mOffset = OFFSET;
-    private static final int MULTIPLIER = 100;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
